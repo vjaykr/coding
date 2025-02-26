c954760964e4aca51db5b8385fcd7665f484ed38
vjaykr <141932646+vjaykr@users.noreply.github.com>
2025-02-26 11:40:12 +0530

    ok

---
+++ SEMESTER_WISE/VI_SEMESTER/IMPACT_WEB/2602/index.js
@@ -0,0 +1,36 @@
+// document.write(10+4)
+// document.write("kaise jo aap ")
+
+// console.log("hello world")
+
+var x = 10;
+var y = 20;
+console.log(x+y,typeof(x+y))
+
+//arithematic
+
+var a = 10;
+var b = 20;
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+//comparison
+var c = 10;
+var d = 20;
+var e = '10';
+console.log("Compression")
+console.log(c===e)
+console.log(c!=d)
+console.log(c>d)
+console.log(c<d)
+console.log(c>=d)
+console.log(c<=e)
+
+//logical
+var e = true;
+var f = false;
+console.log(e&&f)
+console.log(e||f)
